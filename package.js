628a4f45d3d5179b14d1ff5fe680ba30edda46c0
Luca Mussi <ing.luca.mussi@gmail.com>
2015-03-21 10:41:03 +0100

    updated to meteor 1.0.4

--- package.js
+++ package.js
@@ -1,52 +1,56 @@
-Package.describe (
-    {
-        "summary" : "Meteor package to meld user accounts registered with the same email address, or associate many different 3rd-party login services with the same user account."
-    }
-);
-Package.on_use(function (api) {
-    api.use([
-        'accounts-base',
-        'check',
-        'underscore'
-    ], ['server']);
+Package.describe({
+	summary: "Meteor package to link/meld user accounts registered with the same (verified) email address.",
+  version: "1.2.0",
+  name: "splendido:accounts-meld",
+  git: "https://github.com/splendido/meteor-accounts-meld.git",
+});
 
-    api.add_files([
-        'lib/accounts-emails-field/lib/accounts-emails-field.js',
-        'lib/accounts-meld-server.js',
-        'lib/accounts-meld-hooks.js',
-    ], ['server']);
+Package.onUse(function(api) {
+  api.versionsFrom("METEOR@1.0.4");
 
-    api.imply([
-        'accounts-base',
-    ], ['server']);
+	api.use([
+		'accounts-base',
+		'check',
+		'underscore'
+	], ['server']);
 
-    api.export([
-        'AccountsMeld',
-        'MeldActions',
-    ], ['server']);
+	api.add_files([
+		'lib/accounts-emails-field/lib/accounts-emails-field.js',
+		'lib/accounts-meld-server.js',
+		'lib/accounts-meld-hooks.js',
+	], ['server']);
+
+	api.imply([
+		'accounts-base',
+	], ['server']);
+
+	api.export([
+		'AccountsMeld',
+		'MeldActions',
+	], ['server']);
 });
 
 
-Package.on_test(function(api) {
-    api.use('accounts-meld');
+Package.onTest(function(api) {
+	api.use('splendido:accounts-meld');
 
-    api.use([
-        'accounts-base',
-        'accounts-oauth',
-        'accounts-password',
-        'http',
-        'oauth',
-        'oauth-encryption',
-        'oauth2',
-        'random',
-        'service-configuration',
-        'srp',
-        'test-helpers',
-        'tinytest',
-        'underscore'
-    ], ['client', 'server']);
+	api.use([
+		'accounts-base',
+		'accounts-oauth',
+		'accounts-password',
+		'http',
+		'oauth',
+		'oauth-encryption',
+		'oauth2',
+		'random',
+		'service-configuration',
+		'srp',
+		'test-helpers',
+		'tinytest',
+		'underscore'
+	], ['client', 'server']);
 
-    api.add_files([
-        'tests/accounts-meld_tests.js',
-    ], ['client', 'server']);
+	api.add_files([
+		'tests/accounts-meld_tests.js',
+	], ['client', 'server']);
 });
